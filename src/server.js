a6b6b92e2136d923ebb86bd3fa004646f43bfcda
lephuchung <lephuchung003@gmail.com>
2024-02-09 22:56:39 +0700

    video7

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine.js';
+import dotevn from 'dotenv';
+dotevn.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+console.log('check:', port);
 
 configViewEngine(app);
 
